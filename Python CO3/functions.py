eabeb59a7be5c657df2964bad9109e5e12338b51
rahulrmcf <93854467+rahulrmcf@users.noreply.github.com>
2021-12-20 19:43:27 +0530

    Add files via upload

---
+++ Python CO3/functions.py
@@ -0,0 +1,10 @@
+k=45
+def sum(x,y):
+    #return x+y
+    print(x+y)
+    
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return(x**y)
---
+++ Python CO3/math.py
@@ -0,0 +1,15 @@
+import math
+print(math.pi)
+
+import math as m
+print(m.pi)
+
+from math import pi,sqrt
+print(math.pi)
+print(math.sqrt(4))
+
+print(math.cos(0))
+print(math.tan(90))
+print(math.sin(90))
+
+
